825cf1599ced4cb29c306fbf1c65e1332ba02a64
isabelachamorra <isabela.chamorra@gmail.com>
2019-12-04 12:17:01 -0300

    MainViewController refatorada

--- SocialInnovationChallenge/SocialInnovationChallenge/Control/MainViewController.swift
+++ SocialInnovationChallenge/SocialInnovationChallenge/Control/MainViewController.swift
@@ -12,35 +12,19 @@ import FirebaseAuth
 class MainViewController: UIViewController {
     
     //MARK: Properties
+    
     let defaults = UserDefaults.standard
-    var email: String?
-    var password: String?
-    var identifierSegue: String?
     var type: String?
-    @IBOutlet weak var egressButton: UIButton!
-    @IBOutlet weak var companyButton: UIButton!
+    
     
     //MARK: Outlets
-    @IBOutlet weak var segmentControlUser: UISegmentedControl!
+    
+    @IBOutlet weak var egressButton: UIButton!
+    @IBOutlet weak var companyButton: UIButton!
+
     
     //MARK: Views
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        //        if let option = defaults.object(forKey: "option") as? String {
-        //            if option == "egress" {
-        //                performSegue(withIdentifier: "egressSegue", sender: self)
-        //            } else {
-        //                performSegue(withIdentifier: "companySegue", sender: self)
-        //            }
-        //        }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -55,71 +39,47 @@ class MainViewController: UIViewController {
             if option == "egress" {
                 let storyboard = UIStoryboard(name: "RootEgress", bundle: nil)
                 
+                //select the RootCompany view controller
                 vc = storyboard.instantiateViewController(identifier: "HomeEgress")
                 
-            } else {
-                
+            }
+            else {
                 let storyboard = UIStoryboard(name: "RootCompany", bundle: nil)
                 
+                //select the RootEgress view controller
                 vc = storyboard.instantiateViewController(identifier: "HomeCompany")
-                
-                
             }
             
+            //present the selected view controller
             if let window = UIApplication.shared.windows.first {
                 window.rootViewController = vc
                 window.makeKeyAndVisible()
             }
         }
-        
-        
     }
     
     
     //MARK: Actions
     
     @IBAction func tapEgressButton(_ sender: Any) {
-        self.email = "egresso@gmail.com"
-        self.password = "egresso"
-        self.identifierSegue = "egressSegue"
         self.type = "egress"
         
         self.defaults.set(self.type!, forKey: "option")
-        self.performSegue(withIdentifier: self.identifierSegue!, sender: self)
+        self.performSegue(withIdentifier: "egressSegue", sender: self)
     }
     
     
     @IBAction func tapCompanyButton(_ sender: Any) {
-        self.email = "empresa@gmail.com"
-        self.password = "empresa"
-        self.identifierSegue = "companySegue"
         self.type = "company"
         
         self.defaults.set(self.type!, forKey: "option")
-        self.performSegue(withIdentifier: self.identifierSegue!, sender: self)
+        self.performSegue(withIdentifier: "companySegue", sender: self)
         
     }
     
-    //não sei se posso apagar essa parte
-    //    @IBAction func tapEnter(_ sender: Any) {
-    //
-    //        segmentControl()
-    //
-    //        let emailAuth = self.email?.trimmingCharacters(in: .whitespacesAndNewlines)
-    //        let passwordAuth = self.password?.trimmingCharacters(in: .whitespacesAndNewlines)
-    //
-    //        Auth.auth().signIn(withEmail: emailAuth!, password: passwordAuth!) { (result, error) in
-    //            if error != nil {
-    //                print("Senha incorreta")
-    //            } else {
-    //                self.defaults.set(self.type!, forKey: "option")
-    //                self.performSegue(withIdentifier: self.identifierSegue!, sender: self)
-    //            }
-    //        }
-    //    }
-    
     //MARK: Functions
     
+    //add a gradient layer as background - NÃO FUNCIONA AINDA
     func addGradientLayer(){
         let layer = CAGradientLayer()
         layer.frame = self.view.bounds
@@ -130,12 +90,5 @@ class MainViewController: UIViewController {
         
         view.layer.addSublayer(layer)
     }
-    
-    
-    
-    //    let userID = Auth.auth().currentUser!.uid
-    //    override func viewDidDisappear(_ animated: Bool) {
-    //        print("ID: \(userID)")
-    //    }
 }
 
